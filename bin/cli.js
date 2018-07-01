7d8fbc924fe784e17558948b46bccb8a7b308db2
Shawn Presser <shawnpresser@gmail.com>
2018-07-01 16:19:38 -0500

    Fix require

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
+var lib = require("../lib.js");
 _G = {}
 _G.require = require
-dax = require("../lib.js").create(_G)
+module.filename = require("path").resolve("repl");
+module.paths = require("module")._nodeModulePaths(module.filename);
+dax = lib.create(_G)
 dax.main();
 
